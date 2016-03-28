bbd25ed421db7cdedb4cab993fb1a6687ff9e33d
enigmatt-pl <enigmatt.priv@gmail.com>
2016-03-28 13:25:35 +0100

    Adding the organiser feature

---
+++ src/TextBase/DataTables.java
@@ -0,0 +1,173 @@
+package TextBase;
+
+public final class DataTables {
+        public static final String[][] dateDays = 
+        {
+            {
+                "",
+                "first",
+                "second",
+                "third",
+                "fourth",
+                "fifth",
+                "sixth",
+                "seventh",
+                "eightth",
+                "nineth",
+                "tenth",
+                "eleventh",
+                "twelveth",
+                "thirteenth",
+                "fourteenth",
+                "fifteenth",
+                "sixteenth",
+                "seventeenth",
+                "eighteenth",
+                "nineteenth",
+                "twentieth",
+                "twenty first",
+                "twenty second",
+                "twenty third",
+                "twenty fourth",
+                "twenty fifth",
+                "twenty sixth",
+                "twenty seventh",
+                "twenty eightth",
+                "twenty nineth",
+                "thirtieth",
+                "thirty first",
+                                },
+            { 
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
+                "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
+            } 
+        };
+        
+    public static final String[][] dateMonths = 
+    {
+        {
+            "january",
+            "february",
+            "march",
+            "april",
+            "may",
+            "june",
+            "july",
+            "august",
+            "september",
+            "october",
+            "november",
+            "december"
+        }, 
+        {
+            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"
+        }
+    };
+    
+    public static final String[][] dateTime = 
+        {
+            {
+                "",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+                "eleven",
+                "twelve",
+                "thirteen",
+                "fourteen",
+                "fifteen",
+                "sixteen",
+                "seventeen",
+                "eighteen",
+                "nineteen",
+                "twenty",
+                "twenty one",
+                "twenty two",
+                "twenty three",
+                "twenty four",
+                "twenty five",
+                "twenty six",
+                "twenty seven",
+                "twenty eight",
+                "twenty nine",
+                "thirty",
+                "thirty one",
+                "thirty two",
+                "thirty three",
+                "thirty five",
+                "thirty six",
+                "thirty seven",
+                "thirty eight",
+                "thirty nine",
+                "fourty",
+                "fourty one",
+                "fourty two",
+                "fourty three",
+                "fourty four",
+                "fourty six",
+                "fourty seven",
+                "fourty eight",
+                "fourty nine",
+                "fifty",
+                "fifty one",
+                "fifty two",
+                "fifty three",
+                "fifty four",
+                "fifty five",
+                "fifty six",
+                "fifty seven",
+                "fifty eight",
+                "fifty nine",
+                                },
+            { 
+                "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21",
+                "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32",
+                "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43",
+                "44", "45", "46", "47", "48", "49", "50", "51", "52", "54", "55",
+                "56", "57", "58", "59"
+            } 
+        };
+    
+    public static final String[][] dateYear = 
+    {
+        {
+            "two thousand and sixteen",
+            "two thousand and seventeen",
+            "two thousand and eighteen",
+            "two thousand and nineteen",
+            "two thousand and twenty",
+            "two thousand and twenty one",
+            "two thousand and twenty two",
+            "two thousand and twenty three",
+            "two thousand and twenty four",
+            "two thousand and twenty five",
+            "two thousand and twenty six",
+            "two thousand and twenty seven" 
+        },
+        
+        {
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025",
+            "2026",
+            "2027",
+        } 
+    };
+    
+}
